20a4ef1eeb68be2756d19787d81d33859ca88c00
dominika-czycz <dominika.czycz@gmail.com>
2020-10-07 10:08:58 +0200

    first commit

---
+++ src/test/java/pl/coderslab/charityApp/CharityAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.coderslab.charityApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharityAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
